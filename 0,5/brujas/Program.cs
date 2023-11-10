dfd31f78160349cd7b6bc53cf4ba71350b6d62f9
MDeBock <102190577+MDeBock@users.noreply.github.com>
2023-11-10 15:43:04 -0300

    .

---
+++ 0,5/brujas/Program.cs
@@ -0,0 +1,61 @@
+﻿// Veterinaria Las Tres Brujas por Matias Sanchez De Bock
+
+string empleada, alimento;
+double dineroEnCaja = 1000, precioComidaGatos = 50, precioComidaPerros = 50, monto;
+int stockComidaGatos = 100, stockComidaPerros = 100, cantidad;
+
+Console.WriteLine("ingrese el nombre de la empleada");
+
+empleada = Console.ReadLine();
+
+Console.WriteLine($"hola {empleada}");
+
+switch(empleada.ToLower()){
+
+    case "andurias":    
+    Console.WriteLine("ingrese la cantidad que desea modificar");
+    monto = double.Parse(Console.ReadLine());
+    if((dineroEnCaja + monto)>= 0){
+        dineroEnCaja += monto;
+    }else{
+        Console.WriteLine("el dinero en caja no es suficiente");
+    }
+    break;
+
+    case "asterios":
+    Console.WriteLine("que alimento desea modificar?");
+    alimento = Console.ReadLine();    
+    Console.WriteLine("ingrese la cantidad que desea modificar?");
+    cantidad = int.Parse(Console.ReadLine());
+    if (alimento.ToLower() == "gatos" && cantidad <= stockComidaGatos){
+        stockComidaGatos -= cantidad;
+    }else if (alimento.ToLower() == "perros" && cantidad <= stockComidaPerros){
+        stockComidaPerros -= cantidad;
+    }else{
+        Console.WriteLine("no vendemos ese alimento o el stock no es suficiente");
+    }
+    break;
+
+    case "penurias":
+    Console.WriteLine("que alimento desea modificar?");
+    alimento = Console.ReadLine();    
+    Console.WriteLine("ingrese la cantidad que desea modificar?");
+    cantidad = int.Parse(Console.ReadLine());
+    if (alimento.ToLower() == "gatos" && (cantidad * precioComidaGatos)<= dineroEnCaja){
+        stockComidaGatos += cantidad;
+        dineroEnCaja -= (cantidad * precioComidaGatos);
+    }else if (alimento.ToLower() == "perros" && (cantidad * precioComidaPerros)<= dineroEnCaja){
+        stockComidaPerros += cantidad;
+        dineroEnCaja -= (cantidad * precioComidaPerros);
+    }else{
+        Console.WriteLine("no vendemos ese alimento o el dinero no es suficiente");
+    }
+    break; 
+    default:
+    Console.WriteLine("ud no es una empleada autorizada");
+    break;   
+}
+
+Console.WriteLine($"en la caja hay ${dineroEnCaja}");
+Console.WriteLine($"el stock de comida para gatos es de: {stockComidaGatos}");
+Console.WriteLine($"el stock de comida para perros es de: {stockComidaPerros}");
---
+++ 1,0/anaargentinasten/Program.cs
@@ -0,0 +1,102 @@
+﻿string fraseIngresada = "No hay nadie que ame el dolor de por sí, que lo busque y quiera tenerlo, simplemente por el hecho de ser dolor";
+
+//Console.WriteLine("ingrese la palabra o frace que quiera traducir");
+
+//string fraseIngresada = Console.ReadLine();
+
+string fraseTraducida = "";
+
+string fraseDepurada = DepurarFrase(fraseIngresada);
+
+string[] listaPalabras = fraseDepurada.ToLower().Split(' ');
+
+foreach (string palabra in listaPalabras){
+    string traduccionPalabra = SegundaRegla(palabra) + PrimeraRelga(palabra) + palabra + TerceraRegla(palabra) + " ";
+    fraseTraducida += traduccionPalabra;
+}
+
+Console.WriteLine(fraseTraducida);
+
+// funciones
+
+string DepurarFrase (string frase){
+
+    // quitar "," "." y "´"
+
+    string resultado = "";
+    string resultadoParcial = "";
+
+    foreach(char c in frase){
+        if (c != ',' && c != '.'){        
+            resultadoParcial += c.ToString();
+        }
+    }
+
+    foreach(char c in resultadoParcial){
+        if (c == 'á'){        
+            resultado = resultado + c.ToString().Replace("á", "a");
+        }else if (c == 'é'){
+            resultado = resultado + c.ToString().Replace("é", "e");
+        }else if (c == 'í'){
+            resultado = resultado + c.ToString().Replace("í", "i");
+        }else if (c == 'ó'){
+            resultado = resultado + c.ToString().Replace("ó", "o");
+        }else if (c == 'ú'){
+            resultado = resultado + c.ToString().Replace("ú", "u");
+        }else{
+            resultado = resultado + c.ToString();
+        }
+    }
+
+    return resultado;
+}
+
+bool EsVocal(char letra){
+    return (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u');
+}
+
+string PrimeraRelga (string palabra){
+    
+    // Si una palabra empieza con vocal, entonces la primera vocal se duplica, a menos que la segunda letra sea también sea una vocal; en ese caso ignoramos la regla.
+
+    string resultado = "";
+
+    if ((palabra.Length > 1) && (EsVocal(palabra.ElementAt(0)) && (!EsVocal(palabra.ElementAt(1))))){
+            resultado = palabra[0].ToString();            
+        }
+    
+    return resultado;
+}
+
+string SegundaRegla (string palabra){
+
+    //Si una palabra tiene más de 6 letras de largo, debemos agregar “an” al principio, antes de primer sufijo, y luego de aplicar la primera regla.
+
+    string resultado = "";
+
+    if (palabra.Length > 6){
+        resultado = "an";
+    }
+
+    return resultado;
+}
+
+string TerceraRegla (string palabra){
+
+    //Si una palabra termina en n, s o vocal, entonces debemos agregar “sten” al final de la palabra, a menos que la vocal sea ‘o’, en ese caso debemos agregar “so” en lugar.
+
+    string resultado = "";       
+
+    int largoPalabra = palabra.Length - 1; 
+       
+    if (palabra.Length > 1){
+        
+        if ((palabra[largoPalabra] == 'n' || palabra[largoPalabra] == 's' || EsVocal(palabra.ElementAt(largoPalabra))) && palabra[largoPalabra] != 'o'){
+            resultado = "sten";
+        }else if(palabra[largoPalabra] == 'o'){
+            resultado = "so";
+        }
+    }
+
+    return resultado;
+}
